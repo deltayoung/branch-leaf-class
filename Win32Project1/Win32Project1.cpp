f7610baa2de8ec0534ab20b8f89b9298389b1709
Calvin <limcw@ihpc.a-star.edu.sg>
2018-07-05 15:28:03 +0800

    Added a functionality to re-execute the classification with a higher intensityEstimation parameter if less than 10% of the points are classified as branches.

--- Win32Project1/Win32Project1.cpp
+++ Win32Project1/Win32Project1.cpp
@@ -3,6 +3,7 @@
 
 #include "stdafx.h"
 #include "Win32Project1.h"
+#include "cVector.h"
 #include "ShapeGenerator.h"
 
 
@@ -40,85 +41,102 @@ WIN32PROJECT1_API int Classify(int itemNr, float intensityEstimation, float neig
 		
 	*/
 
-	int a; 
-	ShapeGenerator sg; 
-	VertexPoint vptemp; 
-	cVector tempVector, max, min, centroid;
+	intensityEstimation -= 0.01f; 
+	int a,  originalSize, fliteredSize;
 
-	
-	// reading from data lasPointArray 
-	for (a = 0; a < itemNr; a++)
+	do
 	{
-		//classArray[a] = 0; // initializing to type 0
+		intensityEstimation += 0.01f; // adding 0.01f each time. 
 
-		vptemp.id = a; 
-		vptemp.intensity = lasPointArray[a].intensity; 
-		tempVector.x() = lasPointArray[a].x;
-		tempVector.y() = lasPointArray[a].y;
-		tempVector.z() = lasPointArray[a].z;
 		
-		vptemp.position.set(tempVector); 
+		ShapeGenerator sg;
+		VertexPoint vptemp;
+		cVector tempVector, max, min, centroid;
 
-		// indicator for the flags 
-		if (lasPointArray[a].isTrunk == 1)
-			vptemp.flag = true;
-		else
-			vptemp.flag = false; 
-		
 
-		if (a == 0) // first 
+		// reading from data lasPointArray 
+		for (a = 0; a < itemNr; a++)
 		{
-			max.set(tempVector);
-			min.set(tempVector);
+			//classArray[a] = 0; // initializing to type 0
+
+			vptemp.id = a;
+			vptemp.intensity = lasPointArray[a].intensity;
+			tempVector.x() = lasPointArray[a].x;
+			tempVector.y() = lasPointArray[a].y;
+			tempVector.z() = lasPointArray[a].z;
+
+			vptemp.position.set(tempVector);
+
+			// indicator for the flags 
+			if (lasPointArray[a].isTrunk == 1)
+				vptemp.flag = true;
+			else
+				vptemp.flag = false;
+
+
+			if (a == 0) // first 
+			{
+				max.set(tempVector);
+				min.set(tempVector);
+			}
+			else
+			{
+				if (tempVector.x() < min.x()) min.x() = tempVector.x();
+				if (tempVector.y() < min.y()) min.y() = tempVector.y();
+				if (tempVector.z() < min.z()) min.z() = tempVector.z();
+
+				if (tempVector.x() > max.x()) max.x() = tempVector.x();
+				if (tempVector.y() > max.y()) max.y() = tempVector.y();
+				if (tempVector.z() > max.z()) max.z() = tempVector.z();
+			}
+			sg.vp.push_back(vptemp);
 		}
-		else
+
+		// recentralizing 
+		centroid = (max + min) / 2.0f;
+
+		for (a = 0; a < sg.vp.size(); a++)
 		{
-			if (tempVector.x() < min.x()) min.x() = tempVector.x();
-			if (tempVector.y() < min.y()) min.y() = tempVector.y();
-			if (tempVector.z() < min.z()) min.z() = tempVector.z();
-
-			if (tempVector.x() > max.x()) max.x() = tempVector.x();
-			if (tempVector.y() > max.y()) max.y() = tempVector.y();
-			if (tempVector.z() > max.z()) max.z() = tempVector.z();
+			sg.vp[a].position -= centroid;
+			sg.vp[a].position.ref = a; // record the index position 
 		}
-		sg.vp.push_back(vptemp); 
-	}
 
-	// recentralizing 
-	centroid = (max + min) / 2.0f;
+		// neighbourhood construction
+		sg.constructNearestNeighboursVer2(7);
+
+		// getting the intensity limit estimation from trunk 
+		float intensityLimit = sg.trunkBasedFiltering(intensityEstimation);
+
+
+		originalSize = sg.vp.size(); // this is the segmented point cloud consisting of just the trees. (no ground, no other objects) 
+
+		// set the lower trunk classifier 
+		for (a = 0; a < sg.lower_trunk_vp.size(); a++)
+			classArray[sg.lower_trunk_vp[a].id] = 40;
+
+
+		// this is the function for the filtering of points 
+		sg.filterPoints(intensityLimit, neighbourhoodEstimation);
+		sg.neighbourDistanceLimit = neighbourhoodEstimation * 0.5f;
+		sg.shortestPathBranchCreation();
+
+
 	
-	for (a = 0; a < sg.vp.size(); a++)
-	{
-		sg.vp[a].position -= centroid;
-		sg.vp[a].position.ref = a; // record the index position 
-	}
+		fliteredSize = sg.vp.size(); 
 
-	// neighbourhood construction
-	sg.constructNearestNeighboursVer2(7);
 
-	// getting the intensity limit estimation from trunk 
-	float intensityLimit = sg.trunkBasedFiltering(intensityEstimation);
-	
-	
-	// set the lower trunk classifier 
-	for (a = 0; a < sg.lower_trunk_vp.size() ; a++)
-		classArray[sg.lower_trunk_vp[a].id] = 40;
-
+		if (fliteredSize >= 0.1f * originalSize)
+		{
+			// pushing the classification results back to classArray
+			for (a = 0; a < sg.vp.size(); a++)
+			{
+				classArray[sg.vp[a].id] = 40;
+			}
+		}
 		
-	// this is the function for the filtering of points 
-	sg.filterPoints(intensityLimit, neighbourhoodEstimation);
-	sg.neighbourDistanceLimit = neighbourhoodEstimation * 0.5f; 
-	sg.shortestPathBranchCreation();
-	
-	
-	// pushing the classification results back to classArray
-	for (a = 0; a < sg.vp.size(); a++)
-	{
-		classArray[sg.vp[a].id] = 40;
-	}
-	
-		 
-  return 0; 
+	} while (fliteredSize < 0.1f * originalSize);
+
+	return 0; 
 }
 
 
